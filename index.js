c526cd41bed80b9c3530317d9eef898ca6eb1286
Iyanuoluwa Pelumi <ojoiyanuoluwa1@gmail.com>
2025-06-25 00:03:15 +0100

    All images optimised, JavaScript Scroll Sidebar

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 function showSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "flex";
+
+  // To Add scroll event listener when sidebar is shown
+  window.addEventListener("scroll", hideSidebarOnScroll);
 }
+
 function hideSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "none";
+
+  // Remove scroll event listener after sidebar is hidden
+  window.removeEventListener("scroll", hideSidebarOnScroll);
+}
+
+function hideSidebarOnScroll() {
+  hideSidebar();
 }
 
 function redirect() {
